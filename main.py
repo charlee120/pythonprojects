bc9ae59e4b7eeb39bd8165d8b32324851f2a2e05
charlee120 <85306877+charlee120@users.noreply.github.com>
2021-06-06 20:43:37 +0530

    Rename main.py to mirror of the string

--- main.py
+++
@@ -1,3 +0,0 @@
-str=input("Enter a number:")
-print("The given string",str)
-print("The mirror image of the of the give string is ",str[::-1])
---
+++ mirror of the string
@@ -0,0 +1,3 @@
+str=input("Enter a number:")
+print("The given string",str)
+print("The mirror image of the of the give string is ",str[::-1])
