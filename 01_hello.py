e5af0e2708c6f76756ab960666300fe1f86983f7
charlee120 <85306877+charlee120@users.noreply.github.com>
2021-06-09 12:03:43 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+'''
+Author: rakesh
+Licenced to: ABC Company
+***********Thanks for reading**********
+'''
+import os # importing the os module 
+print("Hello world")
---
+++ 04_pr_04_list_directory.py
@@ -0,0 +1,6 @@
+# Author: rakesh
+# Location: Mars
+# Date: 23/02/2020
+
+import os
+print(os.listdir())
