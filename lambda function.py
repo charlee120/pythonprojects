7205ecf8513330fa1e74849dccdf482c82c5c33c
charlee120 <85306877+charlee120@users.noreply.github.com>
2021-06-10 08:37:45 +0530

    Add files via upload

---
+++ lambda function.py
@@ -0,0 +1,9 @@
+
+
+# lambda function is Anonymous function. It is not decleared in standed manner
+# like def...
+
+#syntax:
+#  variable=kayword(lambda):expresion
+x=lambda x,y:x*10+y
+print(x(7,2))
