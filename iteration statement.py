f74dd8b69632804c41b5de2a2ca4d18ac4ec0145
charlee120 <85306877+charlee120@users.noreply.github.com>
2021-06-10 08:16:38 +0530

    Add files via upload

---
+++ iteration statement.py
@@ -0,0 +1,13 @@
+my_list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+rakesh=iter(my_list)
+
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
+print(next(rakesh))
